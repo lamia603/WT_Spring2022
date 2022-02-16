74f1dbaffd2ddeb5fd2b6c998d0ea5644583c84b
username <user@email.com>
2022-02-17 00:25:29 +0600

    new

---
+++ practice.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+   <title>
+       My Site
+</title>
+</head>
+<bodv>
+chi> hello </h1>
+
+«form action="Control/results.php" method="post">
+Username <input type="text" name= "uname">
+Pasword <input type="password" name="password">
+<input type="submit" name="Submit">
+</form>
+</body>
+</html»
+
